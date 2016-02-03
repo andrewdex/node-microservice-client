9c2126ed6221086f45b151f96c07265bb854482b
Dilusha Gonagala <andrewdexis@gmail.com>
2016-02-03 09:07:43 +0530

    Express Commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,25 @@
-
 angular.module('clientApp', [])
-  .controller('mainController', function($scope) {
-    $scope.hello  = "yey";
+  .controller('mainController', function($scope, $http) {
+    $scope.hello = " Hello Microservice Client !";
+
+
+    $scope.dosomething = function() {
+
+
+      $http({
+        method: 'GET',
+        url: 'http://localhost:8000/api/dosomething/'
+      }).then(function successCallback(response) {
+        console.log(response.data.message);
+        // this callback will be called asynchronously
+        // when the response is available
+        $scope.response = response.data.message;
+
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        $scope.responseError = " An error occured ";
+      });
+
+    }
   });
